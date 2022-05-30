0c487d1e1bea2a86846927e5f820fc65ebaf0b24
qp133 <quangphung133@gmail.com>
2022-05-30 19:20:40 +0700

    update

---
+++ JavaCore/Lesson-1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Xin chào");
+        System.out.println("Tôi tên là ...");
+        System.out.println(20+20);
+    }
+}
